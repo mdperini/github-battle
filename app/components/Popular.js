6c474621feeb0b5e207a69a33ff9d84ddc602a9f
Michael D. Perini <perinimichaeld@yahoo.com>
2019-09-26 22:03:27 -0400

    Initial Commit - from Tyler Mcginnus Course

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos} from '../utils/api';
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
 
 function LanguagesNav({ selected, onUpdateLangauge }) {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -26,13 +27,60 @@ function LanguagesNav({ selected, onUpdateLangauge }) {
     }
 }
 
+function RepoGrid({ repos }) {
+    return (
+       <ul className='grid space-around'>
+           {repos.map((repo, index) => {
+               const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
+               const { login, avatar_url } = owner
+
+               return (
+                   <li key={html_url} className='repo bg_light'>
+                       <h4 className='header-lg center-text'>
+                           #{index + 1 }
+                       </h4>
+                       <img
+                        className='avatar'
+                        src={avatar_url}
+                        alt={`Avatar for ${login}`}
+                        />
+                        <h2 className='center-text'>
+                            <a className='link' href={html_url}>{login}</a>
+                        </h2> 
+                        <ul className='card-list'>
+                            <li>
+                              <FaUser color='rgb(255, 191, 116)' size={22} /> 
+                              <a href={`https://github.com/${login}`}>
+                                  {login}
+                              </a> 
+                            </li>
+                            <li>
+                                <FaStar color='rgb(255, 215, 0)' size={22} />
+                                {stargazers_count.toLocaleString()} stars
+                            </li>
+                            <li>
+                                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                                {forks.toLocaleString()} forks
+                            </li>
+                            <li>
+                               <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} /> 
+                               {open_issues.toLocaleString()} open
+                            </li>
+                        </ul> 
+                   </li>
+               )
+           })}
+       </ul> 
+    )
+}
+
 export default class Popular extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
             selectedLanguage: 'All',
-            repos: null,
+            repos: {},
             error: null
         };
 
@@ -40,46 +88,60 @@ export default class Popular extends React.Component {
         this.isLoading = this.isLoading.bind(this);
     }
  
-    componentDidMount() {
-        this.updateLanguage(this.state.selectedLanguage);
-    }
+        componentDidMount() {
+            this.updateLanguage(this.state.selectedLanguage);
+        }
 
-    updateLanguage(selectedLanguage) {
-        this.setState({
-            selectedLanguage,
-            error: null,
-            repos: null
-    });
-
-    fetchPopularRepos(selectedLanguage) 
-        .then((repos) => this.setState({
-            repos,
-            error: null
-        }))
-        .catch(()=> {
-            console.warn('Error fetching repos: ', error)
+        updateLanguage(selectedLanguage) {
             this.setState({
-                error: `There was an error fetching the repositories.`
-            })
-        })
-    }
+                selectedLanguage,
+                error: null
+            });
 
-    isLoading() {
-        return this.state.repos === null && this.state.error === null
-    }
+            if(!this.state.repos[selectedLanguage]) {
+                fetchPopularRepos(selectedLanguage)
+                    .then((data) => {
+                        this.setState(({repos}) => ({
+                            repos: {
+                                ...repos,
+                                [selectedLanguage]: data
+                            }    
+                        }))
 
-    render() {
-        const { selectedLanguage, repos, error } = this.state
-        return (
-            <React.Fragment>
-             <LanguagesNav 
-                selected={selectedLanguage}
-                onUpdateLangauge={this.updateLanguage}
-             />
-            {this.isLoading() && <p>LOADING</p>} 
-            {error && <p>{error}</p>}
-            {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
-            </React.Fragment>
-        )
-    }
+                })
+                .catch(() => {
+                    console.warn('Error fwetching repos: ', error);
+                    this.setState({
+                        error: `There was an error fetching the repositories.`
+                    })
+                })
+            }
+        }
+    
+
+        isLoading() {
+            const {selectedLanguage, repos, error } = this.state;
+
+            return !repos[selectedLanguage] && error === null
+        }
+
+        render() {
+            const { selectedLanguage, repos, error } = this.state
+         
+            return (
+                <React.Fragment>
+                    <LanguagesNav 
+                        selected={selectedLanguage}
+                        onUpdateLangauge={this.updateLanguage}
+                    />
+                
+                    {this.isLoading() && <p>LOADING</p>} 
+        
+                    {error && <p>{error}</p>}
+        
+                    {repos[selectedLanguage] && 
+                        <RepoGrid repos={repos[selectedLanguage]} />}
+                </React.Fragment>
+            )
+        }
 }
--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
+// Tyler Mcginnis React / JavaScript Courses
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
