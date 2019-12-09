49235892477f2c5fdc7ea259c1e9c76297cae4fa
Michael D. Perini <perinimichaeld@yahoo.com>
2019-12-08 21:21:58 -0500

    working complet version from React course by TM

--- app/components/Hover.js
+++ app/components/Hover.js
@@ -1,33 +1,14 @@
 import React from 'react'
 
 export default class Hover extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            hovering: false
-        }
-
-        this.mouseOver = this.mouseOver.bind(this)
-        this.mouseOut = thus.mouseOut.bind(this)
-    }
-
-    mouseOver() {
-        this.setState({
-            hoving: true
-        })
-    }
-    mouseOut() {
-        this.setState({
-            hovering: false
-        })
-    }
-    render() {
-        return (
-            <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
-                {this.props.children(this.state.hovering)}
-
-            </div>
-        )
-    }
+  state = { hovering: false }
+  mouseOver = () => this.setState({ hovering: true })
+  mouseOut = () => this.setState({ hovering: false })
+  render () {
+    return (
+      <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
+        {this.props.children(this.state.hovering)}
+      </div>
+    )
+  }
 }
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,129 +1,118 @@
 import React from 'react'
 import { battle } from '../utils/api'
-import {FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
+import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
 import Card from './Card'
-
 import PropTypes from 'prop-types'
 import Loading from './Loading'
-import Toolip from './Tooltip'
+import Tooltip from './Tooltip'
+import queryString from 'query-string'
+import { Link } from 'react-router-dom'
 
 function ProfileList ({ profile }) {
-    return (
-        <ul className='card-list'>
-            <li>
-                <FaUser color='rgb(239, 115, 115)' size='{22}' />
-                {profile.name}
-            </li>
-            {profile.location && (
-                <li>
-                    <Toolip text="User's location">
-                        <FaCompass color='rgb(144, 115, 255)' size='{22}' />
-                        {profile.location}                   
-                    </Toolip> 
-                </li>
-            )}
-            {profile.company && (
-                <li>
-                    <Toolip text="User's company">
-                        <FaBriefcase color='#795548' size='{22}' />
-                        {profile.company}                   
-                    </Toolip> 
-                </li>
-            )}
-            <li>
-                <FaUsers color='rgb(129, 195, 248)' size='{22}' />
-                {profile.followers.toLocaleString()} followers
-            </li>
-            <li>
-                <FaUserFriends color='rgb(64, 183, 95)' size='{22}' />
-                {profile.following.toLocaleString()} following
-            </li>
-        </ul>
-    )
+  return (
+    <ul className='card-list'>
+      <li>
+        <FaUser color='rgb(239, 115, 115)' size={22} />
+        {profile.name}
+      </li>
+      {profile.location && (
+        <li>
+          <Tooltip text="User's location">
+            <FaCompass color='rgb(144, 115, 255)' size={22} />
+            {profile.location}
+          </Tooltip>
+        </li>
+      )}
+      {profile.company && (
+        <li>
+          <Tooltip text="User's company">
+            <FaBriefcase color='#795548' size={22} />
+            {profile.company}
+          </Tooltip>
+        </li>
+      )}
+      <li>
+        <FaUsers color='rgb(129, 195, 245)' size={22} />
+        {profile.followers.toLocaleString()} followers
+      </li>
+      <li>
+        <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+        {profile.following.toLocaleString()} following
+      </li>
+    </ul>
+  )
 }
 
 ProfileList.propTypes = {
-    profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
 }
 
 export default class Results extends React.Component {
-    constructor(props) {
-        super(props)
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true
+  }
+  componentDidMount () {
+    const { playerOne, playerTwo } = queryString.parse(this.props.location.search)
 
-        this.state = {
-            winner: null,
-            loser: null,
-            error: null,
-            loading: true
-        }
+    battle([ playerOne, playerTwo ])
+      .then((players) => {
+        this.setState({
+          winner: players[0],
+          loser: players[1],
+          error: null,
+          loading: false
+        })
+      }).catch(({ message }) => {
+        this.setState({
+          error: message,
+          loading: false
+        })
+      })
+  }
+  render() {
+    const { winner, loser, error, loading } = this.state
+
+    if (loading === true) {
+      return <Loading text='Battling' />
     }
 
-    componentDidMount() {
-        const { playerOne, playerTwo } = this.props
-        
-        battle([ playerOne, playerTwo ])
-            .then((players) => {
-                this.setState({
-                    winner: players[0],
-                    loser: players[1],
-                    error: null,
-                    loading: false
-                })
-            }).catch(({ message }) => {
-                this.setState({
-                    error: message,
-                    loading: false
-                })
-            })
+    if (error) {
+      return (
+        <p className='center-text error'>{error}</p>
+      )
     }
 
-    render () {
-        const { winner, loser, error, loading } = this.state
-
-        if (loading === true ) {
-            return <Loading text='Battling' />
-        }
-
-        if (error) {
-            return (
-                <p className='center-text error'>{error}</p>
-            )
-        }
-
-        return (
-            <React.Fragment>
-                <div className='grid space-around container-sm'>
-                    <Card
-                        header={winner.score === loser.scope ? 'Tie' : 'Winner'}
-                        subheader={`Score: ${winner.score.toLocaleString()}`}
-                        avatar={winner.profile.avatar_url}
-                        href={winner.profile.html_url}
-                        name={winner.profile.login}
-                        >
-                            <ProfileList profile={winner.profile} />
-                    </Card>
-                    <Card
-                        header={winner.score === loser.scope ? 'Tie' : 'Loser'}
-                        subheader={`Score: ${loser.score.toLocaleString()}`}
-                        avatar={loser.profile.avatar_url}
-                        href={loser.profile.html_url}
-                        name={loser.profile.login}
-                        >
-                            <ProfileList profile={loser.profile} />
-                    </Card>
-                </div>
-                <button 
-                    onClick={this.props.onReset}
-                    className='btn dark-btn btn-space'>
-                        Reset
-                </button>
-            </React.Fragment>
-        )
-    }
-}
-
-releaseEvents.propTypes = {
-    playerOne: PropTypes.string.isRequired,
-    playerTwo: PropTypes.string.isRequired,
-    onReset: PropTypes.func.isRequired
+    return (
+      <React.Fragment>
+        <div className='grid space-around container-sm'>
+          <Card
+            header={winner.score === loser.score ? 'Tie' : 'Winner'}
+            subheader={`Score: ${winner.score.toLocaleString()}`}
+            avatar={winner.profile.avatar_url}
+            href={winner.profile.html_url}
+            name={winner.profile.login}
+          >
+            <ProfileList profile={winner.profile}/>
+          </Card>
+          <Card
+            header={winner.score === loser.score ? 'Tie' : 'Loser'}
+            subheader={`Score: ${loser.score.toLocaleString()}`}
+            avatar={loser.profile.avatar_url}
+            name={loser.profile.login}
+            href={loser.profile.html_url}
+          >
+            <ProfileList profile={loser.profile}/>
+          </Card>
+        </div>
+        <Link
+          to='/battle'
+          className='btn dark-btn btn-space'>
+            Reset
+        </Link>
+      </React.Fragment>
+    )
+  }
 }
